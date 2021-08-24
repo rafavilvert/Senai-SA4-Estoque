4e6ad692e228a5229758a42a5b3b9ffd30cbc934
DarioDistaso <dariodistaso@homtail.com>
2021-08-24 11:58:30 -0300

    melhorias na classe Conexao

--- src/main/java/utils/Conexao.java
+++ src/main/java/utils/Conexao.java
@@ -1,25 +1,33 @@
 package utils;
 
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Conexao {
 
+    final static String DRIVER = "com.mysql.cj.jdbc.Driver";
+    final static String URL = "jdbc:mysql://ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/oeefmopo2ae8bf48";
+    final static String USUARIO = "loquh18ptw5ursa7";
+    final static String SENHA = "qdm4zqrp7uigcdfz";
+    private static Connection conexao;
+
     public static Connection getConexao() {
-        
+
         try {
-            String URL = "jdbc:mysql://ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/oeefmopo2ae8bf48";
-            String USUARIO = "loquh18ptw5ursa7";
-            String SENHA = "qdm4zqrp7uigcdfz";
-            
-            Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
-            System.out.println("Conectado ao banco de dados com sucesso!");
+            if (conexao == null || conexao.isClosed()) {
+                Class.forName(DRIVER);
+                conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+                System.out.println("Conectado ao banco de dados com sucesso!");
+            }
             return conexao;
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
-        
     }
 }
-
-
